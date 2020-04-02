1ca02fd82838970894eb07ce91aef540d1dfccaa
Yusuke Kawamoto <novogrammer@gmail.com>
2020-04-02 18:56:44 +0900

    jestによるtestを追加。

---
+++ test/stylelint.test.js
@@ -0,0 +1,10 @@
+const stylelint = require('stylelint');
+
+// なんか時間がかかるから
+jest.setTimeout(20 * 1000);
+test('`npm_scripts/index.scss` errored to be true', () => {
+  const files = 'test/npm_scripts/index.scss';
+  return stylelint.lint({ files }).then((resultObject) => {
+    expect(resultObject.errored).toBe(true);
+  });
+});
