f6eaff19d550bb8a6d6c7859958f0ac76d36cd5c
Yusuke Kawamoto <novogrammer@gmail.com>
2022-06-02 06:10:51 +0900

    npm run fix

--- test/eslint.test.js
+++ test/eslint.test.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-undef */
-const { CLIEngine } = require('eslint');
+const { CLIEngine } = require("eslint");
 
 const cli = new CLIEngine({
   useEslintrc: true,
 });
 
-test('`npm_scripts/index.es6` errorCount to be 1', () => {
-  const report = cli.executeOnFiles(['test/npm_scripts/index.es6']);
+test("`npm_scripts/index.es6` errorCount to be 1", () => {
+  const report = cli.executeOnFiles(["test/npm_scripts/index.es6"]);
   expect(report.errorCount).toBe(1);
 });
--- test/stylelint.test.js
+++ test/stylelint.test.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-undef */
-const stylelint = require('stylelint');
+const stylelint = require("stylelint");
 
 // なんか時間がかかるから
 jest.setTimeout(20 * 1000);
-test('`npm_scripts/index.scss` errored to be true', () => {
-  const files = 'test/npm_scripts/index.scss';
+test("`npm_scripts/index.scss` errored to be true", () => {
+  const files = "test/npm_scripts/index.scss";
   return stylelint.lint({ files }).then((resultObject) => {
     expect(resultObject.errored).toBe(true);
   });
