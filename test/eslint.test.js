182f46b483919f9bc468c6d46cd32a37cdf746bf
Yusuke Kawamoto <novogrammer@gmail.com>
2022-06-02 06:16:09 +0900

    テストも変更

--- test/eslint.test.js
+++ test/eslint.test.js
@@ -5,7 +5,7 @@ const cli = new CLIEngine({
   useEslintrc: true,
 });
 
-test("`npm_scripts/index.es6` errorCount to be 1", () => {
-  const report = cli.executeOnFiles(["test/npm_scripts/index.es6"]);
+test("`npm_scripts/index.js` errorCount to be 1", () => {
+  const report = cli.executeOnFiles(["test/npm_scripts/index.js"]);
   expect(report.errorCount).toBe(1);
 });
